a27e067c04ac05455f112a323e7836093b182b63
rfromi <romain.fromi@gmail.com>
2019-04-18 10:43:25 +0200

    Bug fix board display

--- src/not-alone/components/OngoingGame.js
+++ src/not-alone/components/OngoingGame.js
@@ -5,9 +5,7 @@ import "./ongoing-game.css"
 const OngoingGame = ({game}) => (
   <div className="ongoing-game">
     <div className="game-zone"/>
-    <div className="board-wrapper">
-      <Board side={game.boardSide} rotated={true}/>
-    </div>
+    <Board side={game.boardSide} rotated={true}/>
   </div>
 )
 
