7b933786d2b4b630073d727fe33c698f2b81f7f4
Romain Fromi <romain.fromi@gmail.com>
2019-08-28 09:50:15 +0200

    Implement Pass button

--- src/not-alone/NotAloneUI.js
+++ src/not-alone/NotAloneUI.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./not-alone.scss"
-import {BOARD_SIDES, CREATURE, HUNT_TOKENS} from "./NotAlone"
+import {BOARD_SIDES, CREATURE, getLegalMoves, HUNT_TOKENS} from "./NotAlone"
 import Board from "./components/Board"
 import {CHOOSE_BOARD_SIDE} from "./moves/ChooseBoardSide"
 import Artemia from "./components/Artemia"
@@ -20,9 +20,21 @@ import {PlaceHuntTokenDisplay} from "./moves-display/PlaceHuntTokenDisplay";
 import HuntToken from "./components/HuntToken";
 import {PASS} from "./moves/Pass";
 import {PassDisplay} from "./moves-display/PassDisplay";
+import Button from "@material-ui/core/Button";
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+import {lightBlue, pink} from "@material-ui/core/colors";
+
+const createTheme = (color) => createMuiTheme({
+  palette: {
+    primary: color
+  },
+  typography: {
+    useNextVariants: true,
+  }
+});
 
 export const Interface = (props) => {
-  const {playerId, game, animation, information} = props
+  const {playerId, game, animation, information, play} = props
 
   const classes = ['not-alone']
   if (!playerId) {
@@ -43,18 +55,22 @@ export const Interface = (props) => {
   }
 
   return (
-    <div className={classes.join(' ')}>
-      <h2 className="information">{information}</h2>
-      {BOARD_SIDES.map(side =>
-        <Board side={side} key={side} {...props}/>
-      )}
-      <Artemia {...props}/>
-      <OtherPlayers {...props}/>
-      <HuntCardsDeck {...props}/>
-      <SurvivalCardsDeck {...props}/>
-      {playerId && <PlayerMaterial {...props}/>}
-      {HUNT_TOKENS.map(token => <HuntToken token={token} locations={game.huntTokensLocations[token]} playerId={playerId} game={game} key={token}/>)}
-    </div>
+    <MuiThemeProvider theme={playerId === CREATURE ? createTheme(pink) : createTheme(lightBlue)}>
+      <div className={classes.join(' ')}>
+        <h2 className="information">{information}</h2>
+        {playerId && getLegalMoves(game, playerId).some(move => move.type === PASS) &&
+        <Button variant="contained" color="primary" size="large" className="pass-button" onClick={() => play({type: PASS, playerId})}>Pass</Button>}
+        {BOARD_SIDES.map(side =>
+          <Board side={side} key={side} {...props}/>
+        )}
+        <Artemia {...props}/>
+        <OtherPlayers {...props}/>
+        <HuntCardsDeck {...props}/>
+        <SurvivalCardsDeck {...props}/>
+        {playerId && <PlayerMaterial {...props}/>}
+        {HUNT_TOKENS.map(token => <HuntToken token={token} locations={game.huntTokensLocations[token]} playerId={playerId} game={game} key={token}/>)}
+      </div>
+    </MuiThemeProvider>
   )
 }
 
