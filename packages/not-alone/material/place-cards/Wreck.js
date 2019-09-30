ab0fe32e93ccc804ed755067837bd055fbcb14dc
Romain Fromi <romain.fromi@gmail.com>
2019-09-30 14:00:10 +0200

    Wreck power

--- packages/not-alone/material/place-cards/Wreck.js
+++ packages/not-alone/material/place-cards/Wreck.js
@@ -1,3 +1,10 @@
+import {moveRescueCounter} from '../../moves/MoveRescueCounter'
+
 export const Wreck = {
-  canUsePower: (game) => !game.wreckUsed
+  canUsePower: (game) => !game.wreckUsed,
+
+  usePower: (game) => {
+    game.nextMoves.push(moveRescueCounter)
+    game.wreckUsed = true
+  }
 }
