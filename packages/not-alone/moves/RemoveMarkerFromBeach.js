669400eedb3b884cdda6cd6dc175deb4e75399e2
Romain Fromi <romain.fromi@gmail.com>
2019-09-30 15:56:57 +0200

    Implement removing marker counter from The Beach

--- packages/not-alone/moves/RemoveMarkerFromBeach.js
+++ packages/not-alone/moves/RemoveMarkerFromBeach.js
@@ -1,6 +1,11 @@
+import {moveRescueCounter} from './MoveRescueCounter'
+
 export const REMOVE_MARKER_FROM_BEACH = 'RemoveMarkerFromBeach'
 export const removeMarkerFromBeach = (huntedId) => ({type: REMOVE_MARKER_FROM_BEACH, huntedId})
 
 export const RemoveMarkerFromBeach = {
-
+  execute: (game) => {
+    game.markerCounterOnBeach = false
+    game.nextMoves.push(moveRescueCounter)
+  }
 }
