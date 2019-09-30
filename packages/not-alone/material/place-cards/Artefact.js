a0e850cfa9ff725053528e3af3702c475aef82be
Romain Fromi <romain.fromi@gmail.com>
2019-09-30 10:49:26 +0200

    River and Artefact activation

--- packages/not-alone/material/place-cards/Artefact.js
+++ packages/not-alone/material/place-cards/Artefact.js
@@ -1,5 +1,12 @@
-import {THE_RIVER} from "../PlaceCards";
+import {getHunted} from '../../NotAlone'
+import {THE_ARTEFACT} from '../PlaceCards'
+import {continueReckoning} from '../../phases/Reckoning'
 
 export const Artefact = {
-  canUsePower: (game, hunted) => hunted.pendingPlacePower !== THE_RIVER
+  canUsePower: (game, hunted) => !hunted.pendingPlacePower,
+
+  usePower: (game, huntedId) => {
+    getHunted(game, huntedId).pendingPlacePower = THE_ARTEFACT
+    continueReckoning(game)
+  }
 }
--- packages/not-alone/material/place-cards/River.js
+++ packages/not-alone/material/place-cards/River.js
@@ -1,7 +1,14 @@
-import {THE_ARTEFACT, THE_RIVER} from "../PlaceCards"
+import {THE_RIVER} from '../PlaceCards'
+import {getHunted} from '../../NotAlone'
+import {continueReckoning} from '../../phases/Reckoning'
 
 export const River = {
-  canUsePower: (game, hunted) => hunted.pendingPlacePower !== THE_ARTEFACT
+  canUsePower: (game, hunted) => !hunted.pendingPlacePower,
+
+  usePower: (game, huntedId) => {
+    getHunted(game, huntedId).pendingPlacePower = THE_RIVER
+    continueReckoning(game)
+  }
 }
 
 export const mustChoosePlaceToReveal = hunted => hunted.pendingPlacePower === THE_RIVER && hunted.playedPlaceCards.length > 1
