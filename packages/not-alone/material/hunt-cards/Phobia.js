1e6775dd813f1a6ed8a9bc1dbbdda73d5c77fcd3
Romain Fromi <romain.fromi@gmail.com>
2019-10-01 18:38:08 +0200

    Phobia rule

--- packages/not-alone/material/hunt-cards/Phobia.js
+++ packages/not-alone/material/hunt-cards/Phobia.js
@@ -1,7 +1,44 @@
 import {ARTEMIA_TOKEN} from '../HuntTokens'
-import {HUNTING} from '../../NotAlone'
+import {getHunted, getHuntedId, HUNT_CARD, HUNTING} from '../../NotAlone'
+import {ASCENDANCY} from '../HuntCards'
+import {chooseHunted} from '../../moves/ChooseHunted'
+import {choosePlace} from '../../moves/ChoosePlace'
+
+const PLACES_TO_KEEP_SECRET = 2
 
 export const Phobia = {
   phase: HUNTING,
-  token: ARTEMIA_TOKEN
+  token: ARTEMIA_TOKEN,
+
+  canBePlayed: game => game.phase === HUNTING && game.hunted.some(hunted => hunted.handPlaceCards.length > PLACES_TO_KEEP_SECRET),
+
+  playCard: game => game.ongoingAction = {cardType: HUNT_CARD, card: ASCENDANCY},
+
+  getCreatureMoves: game => {
+    if (!game.ongoingAction.huntedId) {
+      return game.hunted.filter(hunted => hunted.handPlaceCards.length > PLACES_TO_KEEP_SECRET).map(hunted => chooseHunted(getHuntedId(game, hunted)))
+    } else {
+      return []
+    }
+  },
+
+  chooseHunted: (game, huntedId) => {
+    game.ongoingAction.huntedId = huntedId
+    game.ongoingAction.revealedCards = []
+  },
+
+  getHuntedMoves: (game, huntedId) => {
+    if (huntedId === game.ongoingAction.huntedId) {
+      return getHunted(game, huntedId).handPlaceCards.filter(place => !game.ongoingAction.revealedCards.includes(place)).map(place => choosePlace(huntedId, place))
+    } else {
+      return []
+    }
+  },
+
+  choosePlace: (game, huntedId, place) => {
+    game.ongoingAction.revealedCards.push(place)
+    if (game.ongoingAction.revealedCards.length === getHuntedId(game.ongoingAction.huntedId).handPlaceCards.length + PLACES_TO_KEEP_SECRET) {
+      delete game.ongoingAction
+    }
+  }
 }
--- packages/not-alone/moves/ChoosePlace.js
+++ packages/not-alone/moves/ChoosePlace.js
@@ -1,4 +1,5 @@
-import {getOngoingActionRule} from '../NotAlone'
+import {CREATURE, getOngoingActionRule} from '../NotAlone'
+import {FIERCENESS, PHOBIA} from '../material/HuntCards'
 
 export const CHOOSE_PLACE = 'ChoosePlace'
 
@@ -7,5 +8,16 @@ export const choosePlace = (huntedId, place) => ({type: CHOOSE_PLACE, huntedId, 
 export const ChoosePlace = {
   execute: (game, move) => getOngoingActionRule(game).choosePlace(game, move.huntedId, move.place),
 
-  getView: (move, playerId) => playerId !== move.huntedId ? {...move, place: {}} : move,
+  getView: (move, playerId, game) => {
+    if (playerId === move.huntedId) {
+      return move
+    } else if (ChoosePlace.hideChosenPlace(game, playerId)) {
+      return {...move, place: {}}
+    }
+    return move
+  },
+
+  hideChosenPlace: (game, playerId) => {
+    return game.ongoingAction && (game.ongoingAction.card === FIERCENESS || game.ongoingAction.card === PHOBIA && playerId !== CREATURE)
+  }
 }
