831564eab2fd28f90823f3af9fa7ae7fe99d764b
Romain Fromi <romain.fromi@gmail.com>
2019-10-02 22:57:19 +0200

    Rules for Hologram and Wrong Track

--- packages/not-alone/material/survival-cards/Hologram.js
+++ packages/not-alone/material/survival-cards/Hologram.js
@@ -1,5 +1,21 @@
-import {RECKONING} from '../../NotAlone'
+import {getAdjacentPlaces, RECKONING, SURVIVAL_CARD} from '../../NotAlone'
+import {ARTEMIA_TOKEN} from '../HuntTokens'
+import {HOLOGRAM} from '../SurvivalCards'
+import {placeHuntToken} from '../../moves/PlaceHuntToken'
 
 export const Hologram = {
-  phase: RECKONING
+  phase: RECKONING,
+
+  canBePlayed: game => game.phase === RECKONING && game.huntTokensLocations[ARTEMIA_TOKEN].length > 0,
+
+  playCard: (game, huntedId) => game.ongoingAction = {cardType: SURVIVAL_CARD, card: HOLOGRAM, huntedId},
+
+  getHuntedMoves: (game, huntedId) => {
+    if (game.ongoingAction.huntedId === huntedId) {
+      const artemiaLocations = game.huntTokensLocations[ARTEMIA_TOKEN]
+      return artemiaLocations.flatMap(locations => locations.map(getAdjacentPlaces)).filter(place => !artemiaLocations.includes(place)).map(place => placeHuntToken(ARTEMIA_TOKEN, [place]))
+    } else {
+      return []
+    }
+  }
 }
--- packages/not-alone/material/survival-cards/WrongTrack.js
+++ packages/not-alone/material/survival-cards/WrongTrack.js
@@ -1,5 +1,19 @@
-import {RECKONING} from '../../NotAlone'
+import {getAdjacentPlaces, RECKONING, SURVIVAL_CARD} from '../../NotAlone'
+import {CREATURE_TOKEN} from '../HuntTokens'
+import {placeHuntToken} from '../../moves/PlaceHuntToken'
+import {WRONG_TRACK} from '../SurvivalCards'
 
 export const WrongTrack = {
-  phase: RECKONING
+  phase: RECKONING,
+
+  playCard: (game, huntedId) => game.ongoingAction = {cardType: SURVIVAL_CARD, card: WRONG_TRACK, huntedId},
+
+  getHuntedMoves: (game, huntedId) => {
+    if (game.ongoingAction.huntedId === huntedId) {
+      const creatureLocations = game.huntTokensLocations[CREATURE_TOKEN]
+      return creatureLocations.flatMap(locations => locations.map(getAdjacentPlaces)).filter(place => !creatureLocations.includes(place)).map(place => placeHuntToken(CREATURE_TOKEN, [place]))
+    } else {
+      return []
+    }
+  }
 }
