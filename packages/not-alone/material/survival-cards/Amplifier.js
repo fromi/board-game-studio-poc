e47df44046978afba21f6035005fa8382c514c8f
Romain Fromi <romain.fromi@gmail.com>
2019-10-02 23:01:32 +0200

    Rules for Amplifier and Double Back

--- packages/not-alone/material/survival-cards/Amplifier.js
+++ packages/not-alone/material/survival-cards/Amplifier.js
@@ -1,5 +1,11 @@
 import {END_OF_TURN_ACTIONS} from '../../NotAlone'
+import {removeMarkerFromBeach} from '../../moves/RemoveMarkerFromBeach'
+import {moveRescueCounter} from '../../moves/MoveRescueCounter'
 
 export const Amplifier = {
-  phase: END_OF_TURN_ACTIONS
+  phase: END_OF_TURN_ACTIONS,
+
+  canBePlayed: game => game.phase === END_OF_TURN_ACTIONS && game.markerCounterOnBeach,
+
+  playCard: (game, huntedId) => game.nextMoves.push(removeMarkerFromBeach(huntedId), moveRescueCounter)
 }
--- packages/not-alone/material/survival-cards/DoubleBack.js
+++ packages/not-alone/material/survival-cards/DoubleBack.js
@@ -1,5 +1,19 @@
-import {END_OF_TURN_ACTIONS} from '../../NotAlone'
+import {END_OF_TURN_ACTIONS, getHunted, SURVIVAL_CARD} from '../../NotAlone'
+import {DOUBLE_BACK} from '../SurvivalCards'
+import {takeBackPlayedPlace} from '../../moves/TakeBackPlayedPlace'
 
 export const DoubleBack = {
-  phase: END_OF_TURN_ACTIONS
+  phase: END_OF_TURN_ACTIONS,
+
+  canBePlayed: (game, huntedId) => game.phase === END_OF_TURN_ACTIONS && getHunted(game, huntedId).playedPlaceCards.length > 0,
+
+  playCard: (game, huntedId) => game.ongoingAction = {cardType: SURVIVAL_CARD, card: DOUBLE_BACK, huntedId},
+
+  getHuntedMoves: (game, huntedId) => {
+    if (game.ongoingAction.huntedId === huntedId) {
+      return getHunted(game, huntedId).playedPlaceCards.map(place => takeBackPlayedPlace(huntedId, place))
+    } else {
+      return []
+    }
+  }
 }
