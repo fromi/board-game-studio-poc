37a39b4eae3ab83110e7b16ffd15e2b63961a0b7
Romain Fromi <romain.fromi@gmail.com>
2019-10-02 09:21:56 +0200

    Mirage rule

--- packages/not-alone/material/hunt-cards/Mirage.js
+++ packages/not-alone/material/hunt-cards/Mirage.js
@@ -3,5 +3,7 @@ import {HUNTING} from '../../NotAlone'
 
 export const Mirage = {
   phase: HUNTING,
-  token: TARGET_TOKEN
+  token: TARGET_TOKEN,
+  placeTokenOnAdjacentPlaces: true,
+  isPlaceIneffective: (place, game) => game.huntTokensLocations[TARGET_TOKEN].includes(place)
 }
