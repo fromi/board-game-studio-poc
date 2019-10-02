483c739723b72a9384055c92e41277b6a498b314
Romain Fromi <romain.fromi@gmail.com>
2019-10-02 17:16:36 +0200

    Rule for Ingenuity

--- packages/not-alone/material/survival-cards/Ingenuity.js
+++ packages/not-alone/material/survival-cards/Ingenuity.js
@@ -1,5 +1,10 @@
 import {EXPLORATION} from '../../NotAlone'
+import {putMarkerOnBeach} from '../../moves/PutMarkerOnBeach'
 
 export const Ingenuity = {
-  phase: EXPLORATION
+  phase: EXPLORATION,
+
+  canBePlayed: game => game.phase === EXPLORATION && !game.markerCounterOnBeach,
+
+  playCard: (game, huntedId) => game.nextMoves.push(putMarkerOnBeach(huntedId))
 }
